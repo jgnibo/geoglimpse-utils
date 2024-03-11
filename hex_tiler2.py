69d9818505ca7bd2d3c91d891c8557fe0dfeb604
Joshua Pfefferkorn <77121865+jpfeff@users.noreply.github.com>
2024-03-10 23:55:06 -0400

    cleanup

--- hex_tiler2.py
+++
@@ -1,5 +0,0 @@
-import geopandas as gpd
-import h3pandas
-
-#one-liner
-hex_area = gpd.read_file('./area.geojson').h3.polyfill_resample(12).h3.h3_to_geo_boundary().to_file("hex_grid_test.geojson", driver='GeoJSON')
